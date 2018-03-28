d3cd01ec7f123a35ae8124c9d5e8a32b1fcc7826
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-28 01:50:51 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,21 +1,21 @@
-CREATE TABLE projects(
+CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
   funding_goal NUMERIC,
-  start_date DATE,
-  end_date DATE
+  start_date TEXT,
+  end_date TEXT
 );
 
-CREATE TABLE users(
+CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
 );
 
-CREATE TABLE pledges(
+CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
   amount NUMERIC,
-  user_id INTEGER,
-  project_id INTEGER
+  project_id INTEGER,
+  user_id INTEGER
 );
